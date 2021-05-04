69000dc86d20b90d643a1e74612d65638ea74210
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-05-04 12:26:30 -0700

    #175 setup network requests

---
+++ src/network/index.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import Config from "react-native-config";
+
+const BASE_URL = Config.BASE_URL;
+
+export const getAllPlants = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/plants`);
+
+    return response.data;
+  } catch (error) {
+    console.log(error.reponse);
+    return {
+      error: error.response,
+    };
+  }
+};
+
+export const getPlant = async (id) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/plants/${id}`);
+
+    return response.data;
+  } catch (error) {
+    console.log(error.reponse);
+    return {
+      error: error.response,
+    };
+  }
+};
+
+export const getAllWaypoints = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/waypoints`);
+
+    return response.data;
+  } catch (error) {
+    console.log(error.response);
+    return {
+      error: error.response,
+    };
+  }
+};
+
+export const getWaypoint = async (id) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/waypoints/${id}`);
+
+    return response.data;
+  } catch (error) {
+    console.log(error.reponse);
+    return {
+      error: error.response,
+    };
+  }
+};

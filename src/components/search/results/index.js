a6b1503f64b0b8c2731eab8d7c8960db5331e311
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-05-10 15:26:33 -0700

    #231 implement simple search alogorithm

--- src/components/search/results/index.js
+++ src/components/search/results/index.js
@@ -2,13 +2,45 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import SearchInputCtrl from "../../../controllers/search/input/SearchInputCtrl";
 
-export default function SearchResults({ setter }) {
+export default function SearchResults({ setter, filteredResources }) {
   return (
     <View>
       <SearchInputCtrl setter={setter} />
-      <Text>SearchResults</Text>
+      <View style={styles.results}>
+        <View style={styles.listHead}>
+          <Text style={styles.heading}>Search Query</Text>
+          <Text style={styles.subheading}>
+            Results | &nbsp;{filteredResources.length} items&nbsp;
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  results: {
+    backgroundColor: "#f7f7f7",
+    height: "100%",
+  },
+  listHead: {
+    paddingTop: 20,
+    paddingBottom: 20,
+    width: `100%`,
+    paddingRight: 20,
+    paddingLeft: 20,
+    borderBottomColor: "lightgray",
+    borderBottomWidth: 1,
+  },
+  heading: {
+    color: "#333",
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    fontSize: 21,
+  },
+  subheading: {
+    color: "#333",
+    fontSize: 12,
+    marginTop: 7,
+  },
+});
--- src/controllers/search/results/SearchResultsCtrl.js
+++ src/controllers/search/results/SearchResultsCtrl.js
@@ -1,16 +1,100 @@
 import React, { useState, useEffect } from "react";
 import SearchResults from "../../../components/search/results";
+import { getAllWaypoints, getAllPlants } from "../../../network";
 
 export default function SearchResultsCtrl({ resourceType }) {
   const [searchQuery, setSearchQuery] = useState("");
+  const [resources, setResources] = useState([]);
+  const [filteredResources, setFilteredResources] = useState([]);
 
   useEffect(() => {
-    console.log(searchQuery);
+    queryResources();
+  }, []);
+
+  useEffect(() => {
+    filterResources();
   }, [searchQuery]);
 
   const syncQuery = (string) => {
     setSearchQuery(string);
   };
 
-  return <SearchResults setter={syncQuery} />;
+  const queryResources = async () => {
+    let result;
+    switch (resourceType) {
+      case "plants":
+        result = await getAllPlants();
+        break;
+      case "waypoints":
+        result = await getAllWaypoints();
+        break;
+      default:
+        break;
+    }
+
+    if (result.error) return console.log("Error fetching resources");
+    setResources(result);
+  };
+
+  const filterResources = () => {
+    const query = searchQuery.toLowerCase();
+    if (query === "" || query === undefined || query === null)
+      return setFilteredResources(resources);
+    let filteredData = [];
+    let filteredIds = [];
+
+    const filterId = () => {
+      filteredIds = filteredData.map((resrc) => resrc._id);
+    };
+
+    resources.forEach((resource) => {
+      const categories = resource.categories;
+      const tags = resource.tags;
+
+      switch (resourceType) {
+        case "plants":
+          if (filteredIds.includes(resource._id)) break;
+          if (resource.plant_name.toLowerCase().startsWith(`${query}`))
+            filteredData = [...filteredData, resource];
+          filterId();
+          break;
+        case "waypoints":
+          if (filteredIds.includes(resource._id)) break;
+          if (resource.waypoint_name.toLowerCase().startsWith(`${query}`))
+            filteredData = [...filteredData, resource];
+          filterId();
+          break;
+        default:
+          break;
+      }
+
+      categories.forEach((category) => {
+        if (
+          filteredIds.includes(resource._id) ||
+          typeof category.category_name !== "string"
+        )
+          return;
+        if (category.category_name.toLowerCase().startsWith(`${query}`))
+          filteredData = [...filteredData, resource];
+        filterId();
+      });
+
+      tags.forEach((tag) => {
+        if (
+          filteredIds.includes(resource._id) ||
+          typeof tag.tag_name !== "string"
+        )
+          return;
+        if (tag.tag_name.toLowerCase().startsWith(`${query}`))
+          filteredData = [...filteredData, resource];
+        filterId();
+      });
+
+      setFilteredResources(filteredData);
+    });
+  };
+
+  return (
+    <SearchResults setter={syncQuery} filteredResources={filteredResources} />
+  );
 }

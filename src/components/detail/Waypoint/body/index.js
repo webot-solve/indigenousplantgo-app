9fcb6d03719f2f40d7aba9227cf42f21ca2d9f3e
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-05-11 15:13:44 -0700

    #234 install videos on waypoint details

--- src/components/detail/Waypoint/body/index.js
+++ src/components/detail/Waypoint/body/index.js
@@ -4,6 +4,7 @@ import WaypointHeadCtrl from "../../../../controllers/detail/Waypoint/head/Waypo
 import WaypointDescriptions from "../descriptions";
 import WaypointRelated from "../related";
 import Gallery from "../../../gallery";
+import Videos from "../videos";
 
 export default function WaypointBody({ waypoint, topics, showPlant }) {
   return (
@@ -17,9 +18,16 @@ export default function WaypointBody({ waypoint, topics, showPlant }) {
           description={waypoint.description}
           fields={waypoint.custom_fields}
         />
-        <WaypointRelated resources={waypoint.plants} showDetail={showPlant} />
+        {waypoint && waypoint.plants && waypoint.plants.length > 0 ? (
+          <WaypointRelated resources={waypoint.plants} showDetail={showPlant} />
+        ) : null}
+
         {waypoint && waypoint.images && waypoint.images.length > 1 ? (
           <Gallery images={waypoint.images} resourceType="waypoints" />
+        ) : null}
+
+        {waypoint && waypoint.videos && waypoint.videos.length > 0 ? (
+          <Videos videos={waypoint.videos} />
         ) : null}
       </ScrollView>
     </View>

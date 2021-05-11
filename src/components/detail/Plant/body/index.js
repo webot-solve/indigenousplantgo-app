308614a90dcc08783a99fa87c310430fe14552dc
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-05-11 11:08:48 -0700

    #234 plant descriptions

--- src/components/detail/Plant/body/index.js
+++ src/components/detail/Plant/body/index.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { StyleSheet, Text, View, ScrollView } from "react-native";
 import PlantHeadCtrl from "../../../../controllers/detail/Plant/head/PlantHeadCtrl";
+import PlantDescriptions from "../descriptions";
 
 export default function PlantBody({ plant, topics }) {
   return (
-    <ScrollView style={styles.container}>
-      <PlantHeadCtrl plant={plant} topics={topics} />
-      <Text>PlantBody</Text>
-    </ScrollView>
+    <View style={{ paddingBottom: 220 }}>
+      <ScrollView style={styles.container}>
+        <PlantHeadCtrl plant={plant} topics={topics} />
+        <PlantDescriptions
+          description={plant.description}
+          fields={plant.custom_fields}
+        />
+        <Text>PlantBody</Text>
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: 20,
+    paddingBottom: 100,
   },
 });

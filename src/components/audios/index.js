4ef152ffc6119394a9d56ddc7b296612189bde41
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-05-12 14:59:25 -0700

    #238 implement audio controls

--- src/components/audios/index.js
+++ src/components/audios/index.js
@@ -1,19 +1,46 @@
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Audios({ audio, playSound }) {
+export default function Audios({ soundState, handlePlayPause, stopSound }) {
   return (
     <View style={styles.container}>
       <Text>Audios</Text>
-      <TouchableOpacity onPress={playSound}>
-        <Text>Play Sound</Text>
-      </TouchableOpacity>
+      <View style={styles.controls}>
+        <TouchableOpacity style={styles.control} onPress={() => stopSound()}>
+          <Text>Stahp</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.control}
+          onPress={() => handlePlayPause()}
+        >
+          {soundState.isPlaying ? (
+            <Ionicons name="ios-pause" size={48} color="#444" />
+          ) : (
+            <Ionicons name="ios-play-circle" size={48} color="#444" />
+          )}
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
     paddingHorizontal: 15,
+  },
+  albumCover: {
+    width: 250,
+    height: 250,
+  },
+  controls: {
+    flexDirection: "row",
+  },
+  control: {
+    margin: 20,
   },
 });
--- src/controllers/audios/AudiosCtrl.js
+++ src/controllers/audios/AudiosCtrl.js
@@ -3,25 +3,98 @@ import { Audio } from "expo-av";
 import Audios from "../../components/audios";
 
 export default function AudiosCtrl({ audio }) {
-  const [sound, setSound] = useState(null);
-
-  const playSound = async () => {
-    if (!audio || typeof audio !== "object") return;
-    const { sound } = await Audio.Sound.createAsync({
-      uri: audio.audio_file_url,
-    });
-    setSound(sound);
-
-    await sound.playAsync();
+  const initailSoundState = {
+    isPlaying: false,
+    playbackInstance: null,
+    volumn: 1.0,
+    isBuffering: false,
   };
+  const [soundState, setSoundState] = useState(initailSoundState);
 
   useEffect(() => {
-    return sound
+    console.log(soundState);
+  }, [soundState]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: false,
+          interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+          playsInSilentModeIOS: true,
+          interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+          shouldDuckAndroid: true,
+          staysActiveInBackground: true,
+          playThroughEarpieceAndroid: true,
+        });
+
+        await loadAudio();
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+
+    return soundState.playbackInstance
       ? () => {
-          sound.unloadAsync();
+          soundState.playbackInstance.unloadAsync();
+          setSoundState(initialSoundState);
         }
       : undefined;
-  }, [sound]);
+  }, []);
 
-  return <Audios audio={audio} playSound={playSound} />;
+  const loadAudio = async () => {
+    const { isPlaying, volume } = soundState;
+
+    try {
+      const playbackInstance = new Audio.Sound();
+      const source = {
+        uri: audio.audio_file_url,
+      };
+
+      const status = {
+        shouldPlay: isPlaying,
+        volume,
+      };
+
+      await playbackInstance.setOnPlaybackStatusUpdate();
+      await playbackInstance.loadAsync(source, status, false);
+
+      let soundState_ = { ...soundState };
+      soundState_.playbackInstance = playbackInstance;
+      setSoundState(soundState_);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handlePlayPause = async () => {
+    const { isPlaying, playbackInstance } = soundState;
+    isPlaying
+      ? await playbackInstance.pauseAsync()
+      : await playbackInstance.playAsync();
+
+    let soundState_ = { ...soundState };
+    soundState_.isPlaying = !isPlaying;
+
+    setSoundState(soundState_);
+  };
+
+  const stopSound = async () => {
+    const { isPlaying, playbackInstance } = soundState;
+
+    isPlaying ? await playbackInstance.stopAsync() : undefined;
+
+    let soundState_ = { ...soundState };
+    soundState_.isPlaying = !isPlaying;
+
+    setSoundState(soundState_);
+  };
+
+  return (
+    <Audios
+      soundState={soundState}
+      handlePlayPause={handlePlayPause}
+      stopSound={stopSound}
+    />
+  );
 }

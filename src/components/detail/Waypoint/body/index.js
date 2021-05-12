807780cbe270f451e39bfb480240f65aeeb4a56b
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-05-12 15:33:18 -0700

    #238 finish implementing audio player - waypoints

--- src/components/detail/Waypoint/body/index.js
+++ src/components/detail/Waypoint/body/index.js
@@ -5,6 +5,7 @@ import WaypointDescriptions from "../descriptions";
 import WaypointRelated from "../related";
 import Gallery from "../../../gallery";
 import Videos from "../videos";
+import AudiosCtrl from "../../../../controllers/audios/AudiosCtrl";
 
 export default function WaypointBody({ waypoint, topics, showPlant }) {
   return (
@@ -29,6 +30,15 @@ export default function WaypointBody({ waypoint, topics, showPlant }) {
         {waypoint && waypoint.videos && waypoint.videos.length > 0 ? (
           <Videos videos={waypoint.videos} />
         ) : null}
+
+        {waypoint && waypoint.audio_files && waypoint.audio_files.length > 0 ? (
+          <>
+            <Text style={styles.fieldTitle}>Audio Snippets</Text>
+            {waypoint.audio_files.map((audio, index) => (
+              <AudiosCtrl key={index} audio={audio} />
+            ))}
+          </>
+        ) : null}
       </ScrollView>
     </View>
   );
@@ -37,5 +47,12 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: 20,
     paddingBottom: 0,
+  },
+  fieldTitle: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginTop: 20,
+    marginBottom: 4,
+    paddingHorizontal: 15,
   },
 });

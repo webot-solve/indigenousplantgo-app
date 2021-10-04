36207ddd7b681957506febc0243ab98a2d5415bd
webot-solve <webotsolve@gmail.com>
2021-10-04 10:23:34 -0700

    Added LMDetailCtrl: filter topics, pass data

--- src/controllers/detail/LearnMore/LearnMoreDetailCtrl.js
+++ src/controllers/detail/LearnMore/LearnMoreDetailCtrl.js
@@ -2,13 +2,47 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import LearnMoreDetail from "../../../components/detail/LearnMore"
 
-export default function LearnMoreDetailCtrl(){
+export default function LearnMoreDetailCtrl({learnMore}){
+  let isMounted = true;
+  const [topics, setTopics] = useState([]);
+
+  useEffect(() => {
+    concatInfo();
+  }, [learnMore]);
+
+  useEffect(() => {
+    isMounted = true;
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  const concatInfo = () => {
+    if (!isMounted) return;
+    if (!learnMore) return;
+    let concatArray = [];
+
+    const categories = learnMore.categories.map(
+      (category) => category.category_name
+    );
+    const tags = learnMore.tags.map((tag) => tag.tag_name);
+    if (categories && categories.length > 0)
+      concatArray = [...concatArray, ...categories];
+    if (tags && tags.length > 0) concatArray = [...concatArray, ...tags];
+
+    if (isMounted) setTopics(concatArray);
+  };
+
   return(
     <View>
       <Text>Hello from Controller: Detail LearnMore</Text>
       <LearnMoreDetail
-      
+        learnMore={learnMore}
+        topics={topics}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({});

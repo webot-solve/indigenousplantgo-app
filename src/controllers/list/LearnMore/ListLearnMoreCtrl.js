61b3d7e60a8a9332a9f70da58f984b741ea6b354
webot-solve <webotsolve@gmail.com>
2021-10-04 09:09:44 -0700

    Added LearnMore: query data

--- src/controllers/list/LearnMore/ListLearnMoreCtrl.js
+++ src/controllers/list/LearnMore/ListLearnMoreCtrl.js
@@ -1,12 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import ListLearnMore from "../../../components/list/LearnMore";
+import { getAllLearnMore } from "../../../network";
 
 export default function ListLearnMoreCtrl(){
+  let isMounted = true;
+  const [learnMores, setLearnMores] = useState([])
+
+  useEffect(() => {
+    isMounted = true;
+    queryLearnMores();
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+
+  const queryLearnMores = async () => {
+    if (!isMounted) return;
+    const result = await getAllLearnMore();
+    if (result.error) return console.log("Error fetching learn more");
+    if (isMounted) setLearnMores(result);
+  };
+
   return(
-    <View>
-      <Text>Hello from Controller List LearnMore</Text>
-      <ListLearnMore/>
+    <View style={{ flex: 1, flexDirection: "column" }}>
+      <ListLearnMore
+        learnMores={learnMores}
+      />
     </View>
     
 
--- src/screens/List/ListLearnMore.js
+++ src/screens/List/ListLearnMore.js
@@ -1,17 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 // import SearchButton from "../../components/search/button";
-// import ListToursCtrl from "../../controllers/list/Tours/ListToursCtrl"
 import ListLearnMoreCtrl from "../../controllers/list/LearnMore/ListLearnMoreCtrl";
 
 export default function ListLearnMore({ navigation }) {
   return (
     <View style={{ flex: 1, flexDirection: "column" }}>
       {/* <SearchButton navigation={navigation} resourceType="tours" /> */}
-      
-      {/* <ListToursCtrl navigation={navigation} /> */}
-      <Text>Hello from Screen LearnMore</Text>
-      <ListLearnMoreCtrl/>
+      <ListLearnMoreCtrl navigation={navigation}/>
     </View>
   );
 }
